eb622cc5229739aaf9874cc6415299449e3bf635
Gniewko <gniewko@bodora.pl>
2025-05-21 22:13:30 +0200

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,27 +6,32 @@
             <a href="{{ route('kontakt') }}" class="hover:underline">Kontakt</a>
             @auth
                 @if(Auth::user()->role === 'admin')
-                    <a href="{{ route('admin.appointments.index') }}" class="hover:underline">Rezerwacje</a>
                     <a href="{{ route('admin.services.index') }}" class="hover:underline">Usługi (admin)</a>
+                    <a href="{{ route('admin.calendar') }}" class="hover:underline">Kalendarz</a>
                     <a href="{{ route('admin.kontakt') }}" class="hover:underline">Wiadomości</a>
                     <a href="{{ route('admin.users.index') }}" class="hover:underline">Użytkownicy</a>
-                    <a href="{{ route('admin.calendar') }}" class="hover:underline">Kalendarz</a>
+                    {{-- Dodaj jeśli masz trasę: --}}
+                    @if(Route::has('admin.home'))
+                        <a href="{{ route('admin.home') }}" class="hover:underline">Edycja strony głównej</a>
+                    @endif
                 @else
-                    <a href="{{ route('appointments.index') }}" class="hover:underline">Moje wizyty</a>
-                    <a href="{{ route('appointments.create') }}" class="hover:underline">Nowa rezerwacja</a>
+                    <a href="{{ route('appointments.index') }}" class="hover:underline">Moje rezerwacje</a>
+                    {{-- Dopasuj trasę do wiadomości użytkownika --}}
+                    @if(Route::has('messages.index'))
+                        <a href="{{ route('messages.index') }}" class="hover:underline">Wiadomości</a>
+                    @endif
                 @endif
             @endauth
         </div>
-        <div>
+        <div class="flex items-center space-x-4">
             @auth
-                <a href="{{ route('dashboard') }}" class="ml-4 hover:underline">Panel</a>
                 <form method="POST" action="{{ route('logout') }}" class="inline">
                     @csrf
-                    <button type="submit" class="ml-4 text-red-700 hover:underline">Wyloguj</button>
+                    <button type="submit" class="text-red-700 hover:underline">Wyloguj</button>
                 </form>
             @else
                 <a href="{{ route('register') }}" class="hover:underline">Zarejestruj się</a>
-                <a href="{{ route('login') }}" class="ml-4 hover:underline">Zaloguj się</a>
+                <a href="{{ route('login') }}" class="hover:underline">Zaloguj się</a>
             @endauth
         </div>
     </div>
