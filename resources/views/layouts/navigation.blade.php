eacd354fc551a40bba3b908997ca65bfe1603deb
Gniewko <gniewko@bodora.pl>
2025-05-21 22:16:19 +0200

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,6 @@
 <nav class="bg-white border-b">
     <div class="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+        {{-- Lewa strona: główne linki --}}
         <div class="flex items-center space-x-8">
             <a href="{{ route('home') }}" class="font-bold text-lg">Salon Black&White</a>
             <a href="{{ route('uslugi') }}" class="hover:underline">Usługi</a>
@@ -10,19 +11,14 @@
                     <a href="{{ route('admin.calendar') }}" class="hover:underline">Kalendarz</a>
                     <a href="{{ route('admin.kontakt') }}" class="hover:underline">Wiadomości</a>
                     <a href="{{ route('admin.users.index') }}" class="hover:underline">Użytkownicy</a>
-                    {{-- Dodaj jeśli masz trasę: --}}
-                    @if(Route::has('admin.home'))
-                        <a href="{{ route('admin.home') }}" class="hover:underline">Edycja strony głównej</a>
-                    @endif
+                    {{-- Dodaj tutaj link do edycji strony głównej jeśli utworzysz trasę, np. admin.home --}}
                 @else
                     <a href="{{ route('appointments.index') }}" class="hover:underline">Moje rezerwacje</a>
-                    {{-- Dopasuj trasę do wiadomości użytkownika --}}
-                    @if(Route::has('messages.index'))
-                        <a href="{{ route('messages.index') }}" class="hover:underline">Wiadomości</a>
-                    @endif
+                    {{-- Brak trasy do wiadomości użytkownika, możesz dodać tutaj gdy ją zrobisz --}}
                 @endif
             @endauth
         </div>
+        {{-- Prawa strona: logowanie/rejestracja lub wylogowanie --}}
         <div class="flex items-center space-x-4">
             @auth
                 <form method="POST" action="{{ route('logout') }}" class="inline">
