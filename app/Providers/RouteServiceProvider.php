4aa25d49fcb0160241ad187d44f3068b5697ce05
Gniewko Bodora <gniewko@bodora.pl>
2025-05-23 12:01:22 +0200

    add migration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,37 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-<<<<<<< HEAD
-    /**
-     * The path to the "home" route for your application.
-     *
-     * This is used by Laravel authentication to redirect users after login.
-     *
-     * @var string
-     */
-    public const HOME = '/dashboard';
-
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     */
-    public function boot(): void
-    {
-        //
-
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-
-            Route::middleware(['web', 'auth', 'is_admin'])
-                ->prefix('admin')
-                ->group(base_path('routes/admin.php'));
-        });
-    }
-=======
 	/**
 	 * The path to the "home" route for your application.
 	 *
@@ -52,20 +21,13 @@ class RouteServiceProvider extends ServiceProvider
 	 */
 	public function boot(): void
 	{
-		//
-
 		$this->routes(function () {
-			Route::middleware('api')
-				->prefix('api')
-				->group(base_path('routes/api.php'));
-
 			Route::middleware('web')
 				->group(base_path('routes/web.php'));
 
-			Route::middleware(['web', 'auth', 'is_admin'])
-				->prefix('admin')
-				->group(base_path('routes/admin.php'));
+			Route::prefix('api')
+				->middleware('api')
+				->group(base_path('routes/api.php'));
 		});
 	}
->>>>>>> master
 }
