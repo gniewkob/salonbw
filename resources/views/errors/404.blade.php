99b49a35278fdc1f4cee67d06d6ba0a274c3f0d7
Gniewkob <gniewko74@gmail.com>
2025-06-23 23:36:35 +0200

    Add custom 404 page and adjust fallback route

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+<x-guest-layout>
+    <div class="max-w-lg mx-auto py-16 text-center">
+        <h1 class="text-5xl font-bold mb-4">404</h1>
+        <p class="text-xl mb-6">Strona, której szukasz, nie została znaleziona.</p>
+        <a href="{{ route('home') }}" class="text-blue-600 underline">Wróć do strony głównej</a>
+    </div>
+</x-guest-layout>
