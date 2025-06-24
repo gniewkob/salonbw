ac7cdfab16fb9dc6d121a407e262b8b3f51c7bdd
Gniewkob <gniewko74@gmail.com>
2025-06-24 14:26:20 +0200

    Redesign homepage and navigation

---
+++ resources/views/pages/team.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+    <div class="max-w-3xl mx-auto text-center py-20">
+        <h1 class="text-3xl font-bold mb-6">Nasz Zespół</h1>
+        <p>Ta sekcja jest w przygotowaniu.</p>
+    </div>
+</x-guest-layout>
