80dc7bc72b38b4e5867ff5e635c263d876d5b9a7
Gniewkob <gniewko74@gmail.com>
2025-06-25 10:32:14 +0200

    Add ContactInfoTest and update ExampleTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,16 +2,21 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        Http::fake();
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
