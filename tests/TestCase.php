9c5e3444e5dd3805cefd1e265144cad96f1d5e6c
Gniewkob <gniewko74@gmail.com>
2025-06-30 13:58:27 +0200

    Add phone and notification preference to user factory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,12 @@ use Tests\CreatesApplication;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Disable Vite during tests to avoid missing manifest errors
+        $this->withoutVite();
+    }
 }
