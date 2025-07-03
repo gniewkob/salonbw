e08eea55a12d83b3a3c00acf51d131bc418f54f5
Gniewkob <gniewko74@gmail.com>
2025-07-03 11:20:57 +0200

    Add legal policy pages and footer links

---
+++ resources/views/policy/privacy.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+    <div class="max-w-3xl mx-auto py-20 prose">
+        <h1>Polityka prywatności</h1>
+        <p>Treść polityki prywatności będzie tutaj dostępna wkrótce.</p>
+    </div>
+</x-guest-layout>
