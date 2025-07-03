ddb4554f8ec67918eefa61ab2c6ea019747de44a
Gniewkob <gniewko74@gmail.com>
2025-07-03 16:43:09 +0200

    Format legal policy placeholders and test routes

--- resources/views/policy/partials/contact.blade.php
+++ resources/views/policy/partials/contact.blade.php
@@ -1,10 +1,12 @@
 <h2>Kontakt</h2>
-<p>[Nazwa firmy]<br>
-Adres: [adres]<br>
-NIP: [NIP]<br>
-REGON: [REGON]<br>
-KRS: [jeśli dotyczy]<br>
-E-mail: [adres e-mail]<br>
-Telefon: [numer telefonu]<br>
-Formularz kontaktowy: <a href="{{ route('kontakt') }}">Kontakt</a></p>
+<ul>
+    <li>[Nazwa firmy]</li>
+    <li>Adres: [adres]</li>
+    <li>NIP: [NIP]</li>
+    <li>REGON: [REGON]</li>
+    <li>KRS: [jeśli dotyczy]</li>
+    <li>E-mail: [adres e-mail]</li>
+    <li>Telefon: [numer telefonu]</li>
+</ul>
+<p>Formularz kontaktowy: <a href="{{ route('kontakt') }}">Kontakt</a></p>
 <p>Godziny pracy biura: [godziny pracy]</p>
---
+++ tests/Feature/PolicyPagesTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class PolicyPagesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @dataProvider policyRoutes
+     */
+    public function test_policy_pages_are_accessible(string $route): void
+    {
+        Http::fake();
+
+        $response = $this->get($route);
+        $response->assertStatus(200);
+    }
+
+    public static function policyRoutes(): array
+    {
+        return [
+            ['/polityka-prywatnosci'],
+            ['/polityka-cookies'],
+            ['/regulamin'],
+            ['/dane-kontaktowe'],
+            ['/reklamacje'],
+            ['/moje-zgody'],
+        ];
+    }
+}
+
