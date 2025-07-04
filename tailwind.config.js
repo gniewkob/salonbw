35ae3d99d47dde0aaaa0286eef8aaab7facbec92
Gniewkob <gniewko74@gmail.com>
2025-07-04 09:41:48 +0200

    Add Lato and Open Sans fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Lato', 'Open Sans', 'Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
