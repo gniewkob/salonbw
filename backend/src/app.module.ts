e5a1e82f13d1275c4c0af61e9f29dfce55319794
Gniewkob <gniewko74@gmail.com>
2025-07-04 11:51:57 +0200

    Add health endpoint and swagger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    controllers: [AppController, HealthController],
+    providers: [AppService],
 })
 export class AppModule {}
