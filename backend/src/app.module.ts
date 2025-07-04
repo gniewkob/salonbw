81245717c08361b392a6cc402ed460aac823f5ce
Gniewkob <gniewko74@gmail.com>
2025-07-04 11:57:39 +0200

    feat: add Prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 import { HealthController } from './health.controller';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true })],
     controllers: [AppController, HealthController],
-    providers: [AppService],
+    providers: [AppService, PrismaService],
+    exports: [PrismaService],
 })
 export class AppModule {}
