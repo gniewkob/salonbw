db9a2fa78351c35f29439602ca24b29bae64ea2b
Gniewkob <gniewko74@gmail.com>
2025-07-10 09:59:47 +0200

    refactor: extend PrismaClient directly

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,11 +1,8 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient<
-  Prisma.PrismaClientOptions,
-  'query' | 'info' | 'warn' | 'error' | 'beforeExit'
-> implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
