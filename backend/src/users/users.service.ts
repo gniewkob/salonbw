65f4d24e37ba451b7617307772624ed77b0e34e1
Gniewkob <gniewko74@gmail.com>
2025-07-11 16:58:09 +0200

    Use bcrypt for password hashing

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { Role } from './role.enum';
 
