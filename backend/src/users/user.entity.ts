fd7ed1ae4e7d5f9f26ccce3785af6af768064b8d
Gniewkob <gniewko74@gmail.com>
2025-07-11 15:01:17 +0200

    Add UsersModule and user entity

---
+++ backend/src/users/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Role } from './role.enum';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string; // hashed
+
+    @Column()
+    name: string;
+
+    @Column({ type: 'enum', enum: Role })
+    role: Role;
+}
