2fc435347a0d2f9f02a01f3549574b0488887b06
Gniewkob <gniewko74@gmail.com>
2025-07-14 12:30:05 +0200

    store enums as simple text

--- backend/src/catalog/service.entity.ts
+++ backend/src/catalog/service.entity.ts
@@ -9,7 +9,7 @@ export class Service {
     @Column()
     name: string;
 
-    @Column({ nullable: true })
+    @Column({ type: 'varchar', nullable: true })
     description: string | null;
 
     @Column('int')
