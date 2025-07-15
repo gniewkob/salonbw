c1d3dd41a9439bc60e1d6dcc1e0eb501755882a7
Gniewkob <gniewko74@gmail.com>
2025-07-15 22:10:19 +0200

    Add unique index on user email

--- backend/src/employees/employee.entity.ts
+++ backend/src/employees/employee.entity.ts
@@ -1,10 +1,11 @@
-import { Entity, Column, OneToMany } from 'typeorm';
+import { Entity, Column, OneToMany, Index } from 'typeorm';
 import { User } from '../users/user.entity';
 import { EmployeeRole } from './employee-role.enum';
 import { EmployeeCommission } from '../commissions/employee-commission.entity';
 
 // Employee accounts share the same table as regular users
 // but are typed separately for clarity.
+@Index(['email'], { unique: true })
 @Entity('user')
 export class Employee extends User {
     @Column({
