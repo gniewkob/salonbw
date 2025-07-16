a57543626bea766e6fb000c9fde6e7107e2aca55
Gniewkob <gniewko74@gmail.com>
2025-07-16 13:47:42 +0200

    feat: expose log retrieval

--- backend/src/logs/logs.module.ts
+++ backend/src/logs/logs.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Log } from './log.entity';
 import { LogsService } from './logs.service';
+import { LogsController } from './logs.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Log])],
     providers: [LogsService],
+    controllers: [LogsController],
     exports: [LogsService, TypeOrmModule],
 })
 export class LogsModule {}
