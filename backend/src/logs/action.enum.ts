964262d1d0cb92e49033990e66ac6a60ab144523
Gniewkob <gniewko74@gmail.com>
2025-07-16 11:53:47 +0200

    Expand log action enum

--- backend/src/logs/action.enum.ts
+++ backend/src/logs/action.enum.ts
@@ -2,4 +2,10 @@ export enum LogAction {
     CreateAppointment = 'CREATE_APPOINTMENT',
     UpdateService = 'UPDATE_SERVICE',
     LoginFail = 'LOGIN_FAIL',
+    LoginSuccess = 'LOGIN_SUCCESS',
+    RegisterSuccess = 'REGISTER_SUCCESS',
+    CancelAppointment = 'CANCEL_APPOINTMENT',
+    CompleteAppointment = 'COMPLETE_APPOINTMENT',
+    DeleteAppointment = 'DELETE_APPOINTMENT',
+    DeleteService = 'DELETE_SERVICE',
 }
