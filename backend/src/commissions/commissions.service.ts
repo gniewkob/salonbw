97baf2809f0793ca741939a00df8ea312122a0de
Gniewkob <gniewko74@gmail.com>
2025-07-16 16:19:49 +0200

    Remove unused createForAppointment

--- backend/src/commissions/commissions.service.ts
+++ backend/src/commissions/commissions.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CommissionRecord } from './commission-record.entity';
-import { Appointment } from '../appointments/appointment.entity';
 import { CommissionRule, CommissionTargetType } from './commission-rule.entity';
 import { Service } from '../catalog/service.entity';
 
@@ -51,14 +50,4 @@ export class CommissionsService {
         return base ?? service.defaultCommissionPercent ?? 0;
     }
 
-    createForAppointment(appointment: Appointment) {
-        const record = this.repo.create({
-            employee: appointment.employee,
-            appointment,
-            product: null,
-            amount: appointment.service.price,
-            percent: appointment.service.defaultCommissionPercent ?? 0,
-        });
-        return this.repo.save(record);
-    }
 }
