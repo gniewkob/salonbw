fcff047df3e59b4747b0c39e64c64229a3cf6f25
Gniewkob <gniewko74@gmail.com>
2025-07-16 14:04:27 +0200

    feat(auth): support employee roles in decorator and guard

--- backend/src/auth/roles.decorator.ts
+++ backend/src/auth/roles.decorator.ts
@@ -1,5 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from '../users/role.enum';
+import { EmployeeRole } from '../employees/employee-role.enum';
+
+export type AnyRole = Role | EmployeeRole;
 
 export const ROLES_KEY = 'roles';
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: AnyRole[]) => SetMetadata(ROLES_KEY, roles);
