932e5d94f325bfdc382be107036a9acca77f6bcd
Gniewkob <gniewko74@gmail.com>
2025-07-16 17:47:46 +0200

    fix: include commissions module in appointments

--- backend/src/appointments/appointments.module.ts
+++ backend/src/appointments/appointments.module.ts
@@ -14,6 +14,7 @@ import { LogsModule } from '../logs/logs.module';
     imports: [
         TypeOrmModule.forFeature([Appointment, CommissionRecord]),
         forwardRef(() => FormulasModule),
+        CommissionsModule,
         LogsModule,
     ],
 
