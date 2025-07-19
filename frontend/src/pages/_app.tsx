3640cf87073bb3f25c8b7284d7b5723138d417cf
Gniewkob <gniewko74@gmail.com>
2025-07-19 13:39:56 +0200

    Configure production build and add security headers

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ToastProvider } from '@/contexts/ToastContext';
-import '@/app/globals.css';
+import '@/styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <AuthProvider>
-      <ToastProvider>
-        <Component {...pageProps} />
-      </ToastProvider>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <ToastProvider>
+                <Component {...pageProps} />
+            </ToastProvider>
+        </AuthProvider>
+    );
 }
