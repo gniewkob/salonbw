a835f6c4f26a6be39efab7f7976677edd6859a61
Gniewkob <gniewko74@gmail.com>
2025-08-01 00:44:37 +0200

    Add public products controller and admin endpoints

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from './admin/admin.controller';
 import { EmployeeController } from './employee/employee.controller';
+import { PublicController } from './public/public.controller';
 import { ProductsService } from './products.service';
 import { Product } from '../catalog/product.entity';
 import { Sale } from '../sales/sale.entity';
@@ -9,7 +10,7 @@ import { LogsModule } from '../logs/logs.module';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Product, Sale]), LogsModule],
-    controllers: [AdminController, EmployeeController],
+    controllers: [AdminController, EmployeeController, PublicController],
     providers: [ProductsService],
     exports: [TypeOrmModule, ProductsService],
 })
