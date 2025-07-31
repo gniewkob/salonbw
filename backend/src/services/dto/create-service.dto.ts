a8ff1f6d7caf6b02111ec8521f8cbd166e11e6cf
Gniewkob <gniewko74@gmail.com>
2025-07-31 13:35:54 +0200

    Add validation rules to Service DTOs

--- backend/src/services/dto/create-service.dto.ts
+++ backend/src/services/dto/create-service.dto.ts
@@ -1,7 +1,8 @@
-import { IsInt, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, IsString, Length, Max, Min } from 'class-validator';
 
 export class CreateServiceDto {
     @IsString()
+    @Length(2, 80)
     name: string;
 
     @IsOptional()
@@ -9,9 +10,13 @@ export class CreateServiceDto {
     description?: string;
 
     @IsInt()
+    @Min(10)
+    @Max(480)
     duration: number;
 
-    @IsNumber()
+    @IsNumber({ maxDecimalPlaces: 2 })
+    @Min(1)
+    @Max(10000)
     price: number;
 
     @IsOptional()
