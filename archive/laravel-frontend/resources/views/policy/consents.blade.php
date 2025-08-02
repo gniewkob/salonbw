6826b9a0ab8784eb3385dcf1a5d5ddd77c42aeaf
Gniewko <gniewko@bodora.pl>
2025-08-02 14:15:26 +0200

    remove laravel archiv

--- archive/laravel-frontend/resources/views/policy/consents.blade.php
+++
@@ -1,6 +0,0 @@
-<x-guest-layout>
-    <div class="max-w-3xl mx-auto py-20 prose">
-        <h1>Twoje zgody</h1>
-        @include('policy.partials.consents')
-    </div>
-</x-guest-layout>
--- archive/laravel-frontend/resources/views/policy/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<x-guest-layout>
-    <div class="max-w-3xl mx-auto py-20 prose">
-        <h1>Kontakt</h1>
-@include('policy.partials.contact')
-    </div>
-</x-guest-layout>
