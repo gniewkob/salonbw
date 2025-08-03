83c8531344fbd6b8a98b9ee9c019632e53963d72
Gniewkob <gniewko74@gmail.com>
2025-08-03 19:42:42 +0200

    feat: log employee actions

--- backend/src/employees/employees.module.ts
+++ backend/src/employees/employees.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
+import { LogsModule } from '../logs/logs.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User]), LogsModule],
     controllers: [EmployeesController],
     providers: [EmployeesService],
     exports: [TypeOrmModule, EmployeesService],
