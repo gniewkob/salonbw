73a7adb5ae7ccdaafda5a2c7a1b4e7172913742b
Gniewkob <gniewko74@gmail.com>
2025-08-04 22:42:14 +0200

    Ensure seeded passwords are hashed

--- frontend/design/laravel-frontend/database/seeders/AdminUserSeeder.php
+++ frontend/design/laravel-frontend/database/seeders/AdminUserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 // database/seeders/AdminUserSeeder.php
+// Always hash seeded passwords. Hash::make uses bcrypt; explicitly set rounds
+// so future updates don't accidentally store plaintext values.
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -9,15 +11,16 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-	public function run()
-	{
-		User::updateOrCreate(
-			['email' => 'admin@salon-bw.pl'],
-			[
-				'name' => 'Admin',
-				'password' => Hash::make('SuperSecureHasło!123'),
-				'role' => 'admin',
-			]
-		);
-	}
+        public function run()
+        {
+                User::updateOrCreate(
+                        ['email' => 'admin@salon-bw.pl'],
+                        [
+                                'name' => 'Admin',
+                                // Hash password with bcrypt using 10 rounds
+                                'password' => Hash::make('SuperSecureHasło!123', ['rounds' => 10]),
+                                'role' => 'admin',
+                        ]
+                );
+        }
 }
