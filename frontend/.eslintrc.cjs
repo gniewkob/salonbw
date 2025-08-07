87ad19de48af53fc09ac25cf7c94171cda0f8b22
Gniewkob <gniewko74@gmail.com>
2025-08-07 12:51:26 +0200

    chore: use stable Next.js and React

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint'],
+    extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-empty-object-type': 'off',
+        '@typescript-eslint/no-require-imports': 'off',
+        '@typescript-eslint/no-misused-promises': 'error',
+        'no-unused-vars': 'error',
+    },
+};
