2b508254b4274721c15e472cdcdf6bf5e028902b
Gniewkob <gniewko74@gmail.com>
2025-08-10 01:03:57 +0200

    feat(auth): add jwt strategy and protect profile

--- backend/salonbw-backend/src/auth/auth.module.ts
+++ backend/salonbw-backend/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
@@ -15,7 +16,7 @@ import { LocalStrategy } from './local.strategy';
         }),
         UsersModule,
     ],
-    providers: [AuthService, LocalStrategy],
+    providers: [AuthService, LocalStrategy, JwtStrategy],
     controllers: [AuthController],
     exports: [AuthService, UsersModule],
 })
