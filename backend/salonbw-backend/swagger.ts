31f1d6d0d188f404e4739bf2462c00edcf13425c
Gniewkob <gniewko74@gmail.com>
2025-08-09 22:38:40 +0200

    feat: generate OpenAPI spec

---
+++ backend/salonbw-backend/swagger.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'node:fs';
+import { Module } from '@nestjs/common';
+import { AppController } from './src/app.controller';
+import { AppService } from './src/app.service';
+import { HealthController } from './src/health.controller';
+import { UsersController } from './src/users/users.controller';
+import { UsersService } from './src/users/users.service';
+
+@Module({
+  controllers: [AppController, HealthController, UsersController],
+  providers: [
+    AppService,
+    {
+      provide: UsersService,
+      useValue: {
+        findByEmail: async () => null,
+        createUser: async () => ({ id: 0 }),
+      },
+    },
+  ],
+})
+class SwaggerAppModule {}
+
+async function generate() {
+  const app = await NestFactory.create(SwaggerAppModule, { logger: false });
+  const config = new DocumentBuilder()
+    .setTitle('SalonBW API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  writeFileSync('openapi.json', JSON.stringify(document, null, 2) + '\n');
+  await app.close();
+}
+
+void generate();
