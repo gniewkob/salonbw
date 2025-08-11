0ad2c17ea0a0dbc6ea517fdc76c2f490c212c506
Gniewkob <gniewko74@gmail.com>
2025-08-11 17:47:48 +0200

    feat: add products module

---
+++ backend/salonbw-backend/src/products/products.controller.ts
@@ -0,0 +1,59 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../auth/roles.decorator';
+import { RolesGuard } from '../auth/roles.guard';
+import { Role } from '../users/role.enum';
+import { ProductsService } from './products.service';
+import { Product } from './product.entity';
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
+
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.Admin)
+    @Get()
+    findAll(): Promise<Product[]> {
+        return this.productsService.findAll();
+    }
+
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.Admin)
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<Product | null> {
+        return this.productsService.findOne(Number(id));
+    }
+
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.Admin)
+    @Post()
+    create(@Body() body: Product): Promise<Product> {
+        return this.productsService.create(body);
+    }
+
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.Admin)
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() body: Partial<Product>,
+    ): Promise<Product | null> {
+        return this.productsService.update(Number(id), body);
+    }
+
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(Role.Admin)
+    @Delete(':id')
+    remove(@Param('id') id: string): Promise<void> {
+        return this.productsService.remove(Number(id));
+    }
+}
