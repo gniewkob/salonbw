a027e35b9918bfc60bd8c6c50915cb196d15cc89
Gniewkob <gniewko74@gmail.com>
2025-08-14 10:06:57 +0200

    feat: add logs endpoint

--- backend/salonbw-backend/src/logs/logs.module.ts
+++ backend/salonbw-backend/src/logs/logs.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Log } from './log.entity';
 import { LogService } from './log.service';
+import { LogsController } from './logs.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Log])],
+    controllers: [LogsController],
     providers: [LogService],
     exports: [LogService],
 })
