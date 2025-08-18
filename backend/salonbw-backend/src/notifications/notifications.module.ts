13ea20af8aeab923357065cd5eda7cbfea798a85
Gniewkob <gniewko74@gmail.com>
2025-08-19 00:12:48 +0200

    feat: add reminder cron

--- backend/salonbw-backend/src/notifications/notifications.module.ts
+++ backend/salonbw-backend/src/notifications/notifications.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Appointment } from '../appointments/appointment.entity';
 import { WhatsappService } from './whatsapp.service';
+import { ReminderService } from './reminder.service';
 
 @Module({
-    imports: [HttpModule],
-    providers: [WhatsappService],
+    imports: [HttpModule, TypeOrmModule.forFeature([Appointment])],
+    providers: [WhatsappService, ReminderService],
     exports: [WhatsappService],
 })
 export class NotificationsModule {}
