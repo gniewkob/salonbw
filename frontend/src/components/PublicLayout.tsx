1c73ff8cd5e199e1d5f3545a9210cc723b994fbe
Gniewkob <gniewko74@gmail.com>
2025-08-23 22:03:33 +0200

    feat(frontend): add footer component

--- frontend/src/components/PublicLayout.tsx
+++ frontend/src/components/PublicLayout.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { ReactNode } from 'react';
 import PublicNav from './PublicNav';
+import Footer from './Footer';
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
     return (
         <div className="min-h-screen flex flex-col">
             <PublicNav />
             <main className="flex-1 p-4">{children}</main>
+            <Footer />
         </div>
     );
 }
