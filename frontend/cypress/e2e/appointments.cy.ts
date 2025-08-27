b6b8a305b4f03694a010e2aad6fab4cc5f19e1c1
Gniewkob <gniewko74@gmail.com>
2025-08-27 12:37:00 +0200

    test: wait for service select form

--- frontend/cypress/e2e/appointments.cy.ts
+++ frontend/cypress/e2e/appointments.cy.ts
@@ -16,8 +16,9 @@ describe('appointments', () => {
         cy.wait('@getAppointments');
 
         cy.get('[data-date]').first().click();
-
-        cy.get('[data-testid="service-select"]').click({ force: true });
+        cy.get('[data-testid="service-select"]', { timeout: 10000 })
+            .should('exist')
+            .click({ force: true });
         cy.get('[data-radix-collection-item]').contains('Color').click();
         cy.get('[data-testid="service-select"]').contains('Color');
     });
