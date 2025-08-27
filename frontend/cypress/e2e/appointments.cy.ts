d10962a592afd7a2a0aa028f1112a6e8dbe7493d
Gniewkob <gniewko74@gmail.com>
2025-08-27 13:50:42 +0200

    test: wait for service select before choosing option

--- frontend/cypress/e2e/appointments.cy.ts
+++ frontend/cypress/e2e/appointments.cy.ts
@@ -16,9 +16,7 @@ describe('appointments', () => {
         cy.wait('@getAppointments');
 
         cy.get('[data-date]').first().click();
-        cy.get('[data-testid="service-select"]', { timeout: 10000 })
-            .should('exist')
-            .click({ force: true });
+        cy.get('[data-testid="service-select"]', { timeout: 10000 }).should('exist').click({ force: true });
         cy.get('[data-radix-collection-item]').contains('Color').click();
         cy.get('[data-testid="service-select"]').contains('Color');
     });
