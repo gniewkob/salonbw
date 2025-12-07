b2714d92ccf97da5f5fa112d6d6ef43ba1568246
Gniewko <gniewko@bodora.pl>
2025-12-07 21:32:13 +0100

    Cleanup: remove deleted vendor files

--- backend/salonbw-backend/src/chat/dto/join-room.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsInt, IsNotEmpty } from 'class-validator';
-import { Type } from 'class-transformer';
-
-export class JoinRoomDto {
-    @Type(() => Number)
-    @IsInt()
-    @IsNotEmpty()
-    appointmentId: number;
-}
