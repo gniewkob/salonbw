dfc0e279f29334516f9175a58320ade592c2de65
Gniewko <gniewko@bodora.pl>
2026-02-03 14:09:05 +0100

    test(landing): ignore panel-only page tests

--- apps/landing/jest.config.js
+++ apps/landing/jest.config.js
@@ -2,7 +2,13 @@ module.exports = {
     preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/.next/',
+        '/src/__tests__/appointmentsPageActions\\.test\\.tsx',
+        '/src/__tests__/appointmentsPageBranches\\.test\\.tsx',
+        '/src/__tests__/receptionistAppointments\\.test\\.tsx',
+    ],
     extensionsToTreatAsEsm: ['.ts', '.tsx'],
     transform: {
         '^.+\\.(ts|tsx)$': [
